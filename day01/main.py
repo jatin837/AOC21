7261bda698a5aa569fd1e8a3195844bc0df23d77
dj4t9n <pandeyjatin47@gmail.com>
2021-12-01 20:11:40 +0530

    solved day 2

--- day01/main.py
+++ day01/main.py
@@ -11,13 +11,19 @@ def main():
     first = report[0]
     count = 0
     for record in report[1:]:
-        if record > first:
-            print('yes')
-            count += 1
-        else:
-            print('no')
+        count += 1
         first = record
-
     print(count)
 
 
+def main2():
+    report = read_input()
+    count = 0
+    first = report[0] + report[1] + report[2]
+    for i in range(2, len(report)-1):
+        second = report[i-1]+report[i]+report[i+1]
+        if second > first:
+            count += 1
+        first = second 
+
+    print(count)

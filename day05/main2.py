19407135d92fa1756d4a86ae5cece8812b8234cf
dj4t9n <pandeyjatin47@gmail.com>
2021-12-05 15:00:42 +0530

    remove other sourcecode

--- day05/main2.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-inp = sys.stdin
-
-for line in inp:
-    start, end = line.split('->')
-    x1, y1 = tuple(map(int,start.split(',')))
-    x2, y2 = tuple(map(int,end.split(',')))
-    print(x1, y1, x2, y2)

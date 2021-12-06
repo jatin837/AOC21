d733751a8f3355df763d4b166cd0d5ff738beeaf
dj4t9n <pandeyjatin47@gmail.com>
2021-12-06 13:34:01 +0530

    complete day 6

--- day06/main.py
+++ day06/main.py
@@ -1,18 +1,47 @@
 istate = list(map(int, input().split(',')))
+from collections import Counter
 #istate = [3, 4, 3, 1, 2]
 
 def solve(i):
+
     out = istate.copy()
-    for i in range(1, i+1):
-        print('day', i)
-        for j in range(len(out)):
-            if out[j] == 0:
-                out[j] = 6
-                out.append(8)
+    c = Counter()
+
+    for state in istate:
+        c[state] += 1
+
+    for day in range(1, i+1):
+        y = Counter()
+        for k, v in c.items():
+            if k == 0:
+                y[6] += v
+                y[8] += v
             else:
-                out[j] -= 1
-        
-    return out
+                y[k-1] += v
 
+        c = y
 
-print(256, len(solve(256)))
+    return c
+
+####for i in range(1, i+1):
+####    print('day', i)
+####    print(len(out))
+####    for j in range(len(out)):
+####        if out[j] == 0:
+####            out[j] = 6
+####            out.append(8)
+####        else:
+####            out[j] -= 1
+####    
+####return out
+
+print(solve(1))
+print('-'*10)
+print(solve(2))
+
+ans = 0
+for _, v in solve(256).items():
+    ans += v
+
+print('ans', ans)
+#print(256, len(solve(256)))

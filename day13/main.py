b872be761b140d178b80e4f607df09f52db7c052
dj4t9n <pandeyjatin47@gmail.com>
2021-12-13 14:52:54 +0530

    day13 complete

--- day13/main.py
+++ day13/main.py
@@ -1,3 +1,4 @@
+from copy import copy
 with open('input', 'r') as f:
     inp = f.read().split('\n\n')
 
@@ -11,49 +12,55 @@ for line in a.split('\n'):
     ps.append([int(x), int(y)])
 
 
-ins = b[0]
 
-ins = b.split('\n')[0].split()[2].split('=')
-print(ins)
-max_x = 2*int(ins[1])
+inss = b.strip().split('\n')
 
-if ins[0] == 'x':
-    res = list()
-    for i in range(int(max_x//2)):
-        current = list(filter(lambda p:p[0]==i, ps))
-        to_change = list(map(lambda p: [i, p[1]], list(filter(lambda p:p[0]==max_x-i, ps))))
-        print('-'*10)
-        print(current)
-        print(to_change)
-        if len(to_change) != 0:
-            for change in to_change:
-                if change not in current:
-                    res.append(change)
-        for curr in current:
-            res.append(curr)
-        print(len(res))
-        print(res)
-        print('-'*10)
+for ins in inss:
+    d, v = ins.strip().split()[-1].split('=')
+    v = int(v)
 
-
-elif ins[0] == 'y':
-    res = list()
-    for i in range(int(max_x//2)):
-        current = list(filter(lambda p:p[1]==i, ps))
-        to_change = list(map(lambda p: [p[0], i], list(filter(lambda p:p[1]==max_x-i, ps))))
-        print('-'*10)
-        print(current)
-        print(to_change)
-        print('-'*10)
-        if len(to_change) != 0:
-            for change in to_change:
-                if change not in current:
-                    res.append(change)
+    if d == 'x':
+        w = v
+        res = list()
+        for i in range(int(v)):
+            current = list(filter(lambda p:p[0]==i, ps))
+            to_change = list(map(lambda p: [i, p[1]], list(filter(lambda p:p[0]==2*v-i, ps))))
             for curr in current:
                 res.append(curr)
+            if len(to_change) != 0:
+                for change in to_change:
+                    if change not in current:
+                        res.append(change)
+            else:
+                continue
+
+
+    elif d == 'y':
+        l = v
+        res = list()
+        for i in range(int(v)):
+            current = list(filter(lambda p:p[1]==i, ps))
+            to_change = list(map(lambda p: [p[0], i], list(filter(lambda p:p[1]==2*v-i, ps))))
+            for curr in current:
+                res.append(curr)
+            if len(to_change) != 0:
+                for change in to_change:
+                    if change not in current:
+                        res.append(change)
+            else:
+                continue
+    ps = res.copy()
+
+
+
+ans = ''
+for i in range(l):
+    for j in range(w):
+        if [j, i] in ps:
+            ans += 'X'
         else:
-            continue
+            ans += ' '
 
-print(res)
-print(len(res))
+    ans += '\n'
 
+print(ans)

9fe764c8bdff2725aa6941eeaa91213612955378
dj4t9n <pandeyjatin47@gmail.com>
2021-12-14 18:59:54 +0530

    day14 complete

--- day14/main.py
+++ day14/main.py
@@ -8,27 +8,39 @@ with open('input', 'r') as f:
 init_seq, rules = inp.strip().split('\n\n')
 
 rule = {}
+
 for r in rules.split('\n'):
     a, b = r.strip().split('->')
     rule[a.strip()] = b.strip()
 
 
-for s in range(40):
-    print(s)
-    new_seq = ''
-    for i,ch in enumerate(init_seq[:-1]):
-        d = ch + init_seq[i+1]
-        if d in rule.keys():
-            new_seq += ch + rule[d]
+C1 = Counter()
+
+for i,ch in enumerate(init_seq[:-1]):
+    d = ch + init_seq[i+1]
+    C1[d] += 1
+ 
+
+for _ in range(40):
+    C2 = Counter()
+    for k, v in C1.items():
+        if k in rule.keys():
+            C2[k[0] + rule[k]] += v 
+            C2[rule[k] + k[1]] += v
         else:
-            new_seq += ch
-    new_seq += init_seq[-1]
-    init_seq = new_seq
-    print(init_seq)
+            C2[k] += v
+
+    C1 = C2
+
+C3 = Counter()
+for k, v in C1.items():
+    C3[k[0]] += 1*v
+    C3[k[1]] += 1*v
+
+a = max(C3.values())
+b = min(C3.values())
+print(a-b)
+print((a-b)//2)
 
 
-counter = Counter()
-for c in init_seq:
-    counter[c] += 1
-
-print(max(counter.values()) - min(counter.values()))
+# ABC -> AM, MB, BD, DC
